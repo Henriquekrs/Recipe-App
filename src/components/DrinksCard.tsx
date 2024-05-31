da63d0db6ea69f03cbdf4def65e81d5d4c0d6600
henriquekrs <ghrduarte@hotmail.com>
2024-05-30 23:04:43 -0300

    Feat-02: Create components

---
+++ src/components/DrinksCard.tsx
@@ -0,0 +1,14 @@
+const DrinksCards = () => {
+  return (
+    <div>
+      <h1>Drinks</h1>
+      <ul>
+        <li>Water</li>
+        <li>Tea</li>
+        <li>Coffee</li>
+      </ul>
+    </div>
+  )
+};
+
+export default DrinksCards;
