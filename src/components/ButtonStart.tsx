73738da2a937ca2ee953e83bc2b25a378b4c9cd5
henriquekrs <ghrduarte@hotmail.com>
2024-06-03 22:12:03 -0300

    Feat-12: Implementation button

---
+++ src/components/ButtonStart.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/Button.module.css';
+
+const ButtonStart = () => {
+  return (
+    <div className={styles.container}>
+      <button>START RECIPE</button>
+    </div>
+  );
+};
+
+export default ButtonStart;
