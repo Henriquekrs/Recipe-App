2346f784f01638cb44727f0a1728b962469d117f
henriquekrs <ghrduarte@hotmail.com>
2024-07-03 14:19:45 -0300

    Fix problems

--- src/components/RecommendedRecipe.tsx
+++ src/components/RecommendedRecipe.tsx
@@ -1,28 +1,37 @@
-import React, { useEffect } from 'react'
-import { useGlobalContext } from '@/context/GlobalProvider'
-import Image from 'next/image'
-import styles from '@/styles/ContaineRecommendations.module.css'
-import { useRouter } from 'next/router'
+import React, { useEffect } from 'react';
+import { useGlobalContext } from '@/context/GlobalProvider';
+import Image from 'next/image';
+import styles from '@/styles/ContaineRecommendations.module.css';
+import { useRouter } from 'next/router';
+import { FoodRecipe, DrinkRecipe } from '@/types/recipeType';
 
 const RecommendedRecipes = () => {
-  const router = useRouter()
-  const { getMeals, getDrinks, recipes } = useGlobalContext()
+  const router = useRouter();
+  const { getMeals, getDrinks, recipes } = useGlobalContext();
 
   useEffect(() => {
     if (router.pathname.includes('meals')) {
-      getDrinks('')
+      getDrinks('');
     } else if (router.pathname.includes('drinks')) {
-      getMeals('')
+      getMeals('');
     }
-  }, [])
+  }, [router.pathname]);
 
   const handleClickRecipe = (recipeId: string) => {
     if (router.pathname.includes('meals')) {
-      router.push(`/drinks/${recipeId}`)
+      router.push(`/drinks/${recipeId}`);
     } else if (router.pathname.includes('drinks')) {
-      router.push(`/meals/${recipeId}`)
+      router.push(`/meals/${recipeId}`);
     }
-  }
+  };
+
+  const isFoodRecipe = (recipe: any): recipe is FoodRecipe => {
+    return (recipe as FoodRecipe).idMeal !== undefined;
+  };
+
+  const isDrinkRecipe = (recipe: any): recipe is DrinkRecipe => {
+    return (recipe as DrinkRecipe).idDrink !== undefined;
+  };
 
   return (
     <div className={styles.container}>
@@ -31,21 +40,47 @@ const RecommendedRecipes = () => {
         {recipes.slice(0, 10).map((recipe, index) => (
           <li key={index} className={styles.carouselItem}>
             <button
-              onClick={() => handleClickRecipe(recipe.idMeal || recipe.idDrink)}
+              onClick={() =>
+                handleClickRecipe(
+                  isFoodRecipe(recipe)
+                    ? recipe.idMeal
+                    : isDrinkRecipe(recipe)
+                      ? recipe.idDrink
+                      : '',
+                )
+              }
             >
               <Image
                 width={200}
                 height={200}
-                src={recipe.strMealThumb || recipe.strDrinkThumb}
-                alt={`Image of ${recipe.strMeal || recipe.strDrink}`}
+                src={
+                  isFoodRecipe(recipe)
+                    ? recipe.strMealThumb || ''
+                    : isDrinkRecipe(recipe)
+                      ? recipe.strDrinkThumb || ''
+                      : ''
+                }
+                alt={`Image of ${
+                  isFoodRecipe(recipe)
+                    ? recipe.strMeal
+                    : isDrinkRecipe(recipe)
+                      ? recipe.strDrink
+                      : ''
+                }`}
               />
-              <p>{recipe.strMeal || recipe.strDrink}</p>
+              <p>
+                {isFoodRecipe(recipe)
+                  ? recipe.strMeal
+                  : isDrinkRecipe(recipe)
+                    ? recipe.strDrink
+                    : ''}
+              </p>
             </button>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default RecommendedRecipes
+export default RecommendedRecipes;
